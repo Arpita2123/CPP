d4946caa325117bd022c25a9a8869f236a67fe4d
Arpita2123 <159871699+Arpita2123@users.noreply.github.com>
2024-03-18 20:16:59 +0530

    Add files via upload
    operators

---
+++ headerfileAndOperators.cpp
@@ -0,0 +1,23 @@
+#include<iostream>  //click iostream with press the ctrl button to open iostream file.
+using namespace std ;
+
+int main()
+{
+    int a =4 , b =5 ;
+    cout << "Following are the types of operators :" ;
+
+    // Arithmatic Operators :
+    cout << "The value of a + b :" << a+b << endl ;
+    cout << "The value of a - b :" << a-b << endl ;
+    cout << "The value of a * b :" << a*b << endl ;
+    cout << "The value of a / b :" << a/b << endl ;
+    cout << "The value of a % b :" << a%b << endl ;
+    cout << "The value of a++ :" << a++ << endl ;
+    cout << "The value of a-- :" << a-- << endl ;
+    cout << "The value of ++a :" << ++a << endl ;
+    cout << "The value of --a   :" << --a << endl ;
+
+    
+    return 0;
+    
+}
